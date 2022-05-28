66be0077bbd5db039bbfa1d394387e002761d659
Yvan <dev@yvan.dev>
2022-05-28 23:31:36 +0000

    don't mind me i'm just tired

--- src/includes/database.php
+++ src/includes/database.php
@@ -4,7 +4,7 @@ $database_needed = true;
 if(isset($database_needed) and $database_needed == true) {
     // Connect to the database
     try{
-        $db = new PDO('mysql:host='.$db_host.';dbname='.$db_name.';charset=utf8', $db_user, $db_password,
+        $db = new PDO('mysql:host='.$db_host.';dbname='.$db_name.';charset=utf8', $db_user, $db_passwd,
         array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     } catch(PDOException $e) {
         die("Connection failed: ".$e->getMessage());
--- src/includes/login.php
+++ src/includes/login.php
@@ -3,17 +3,6 @@ session_start();
 include("database.php");
 
 if(isset($_POST['login']) and isset($_POST['password'])) {
-    $login = $_POST['login'];
-    $password = $_POST['password'];
-    $query = "SELECT * FROM users WHERE login='$login' AND password='$password'";
-    $result = mysqli_query($db, $query);
-    if(mysqli_num_rows($result) == 1) {
-        $_SESSION['login'] = $login;
-        $_SESSION['password'] = $password;
-        header("Location: index.php");
-    } else {
-        echo "Identifiants incorrects";
-    }
 }
 
 ?>
