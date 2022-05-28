34583e065858eac178ec44b1fd8ce010e11d9128
Yvan <dev@yvan.dev>
2022-05-28 23:51:36 +0000

    ajout des pages de bases

---
+++ src/includes/logout.php
@@ -0,0 +1,8 @@
+<?php
+// On pourrait faire une condition pour mettre en place des tokens CSRF.
+// Pour l'instant, on ne le fait pas.
+
+
+session_destroy();
+
+?>
---
+++ src/product.php
@@ -0,0 +1,12 @@
+<?php
+
+$title = "Page produit";
+$description = "Page produit du site komposant.com";
+
+?>
+<html>
+    <?php include("includes/header.php"); ?>
+
+    <?php include("includes/navbar.php"); ?>
+
+</html>
