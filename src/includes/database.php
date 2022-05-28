bcb828968bf675d9e5698dec3c66202758ca7297
Yvan <dev@yvan.dev>
2022-05-28 23:30:05 +0000

    test

--- src/includes/database.php
+++ src/includes/database.php
@@ -1,11 +1,13 @@
 <?php
 include("config.php");
-
+$database_needed = true;
 if(isset($database_needed) and $database_needed == true) {
     // Connect to the database
-    $db = mysqli_connect($db_host, $db_user, $db_passwd, $db_name);
-    if(!$db) {
-        die("Connection failed: " . mysqli_connect_error());
+    try{
+        $db = new PDO('mysql:host='.$db_host.';dbname='.$db_name.';charset=utf8', $db_user, $db_password,
+        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    } catch(PDOException $e) {
+        die("Connection failed: ".$e->getMessage());
     }
 }
 
