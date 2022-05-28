425840bec4af5201de18a79799c91fd3e41209e4
Yvan <dev@yvan.dev>
2022-05-28 23:53:12 +0000

    oups oublie de push des dernieres modifs

--- src/includes/database.php
+++ src/includes/database.php
@@ -1,14 +1,17 @@
 <?php
 include("config.php");
-$database_needed = true;
-if(isset($database_needed) and $database_needed == true) {
-    // Connect to the database
-    try{
-        $db = new PDO('mysql:host='.$db_host.';dbname='.$db_name.';charset=utf8', $db_user, $db_passwd,
-        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-    } catch(PDOException $e) {
-        die("Connection failed: ".$e->getMessage());
-    }
+
+// Connexion à la base de données grâce à PDO
+try{
+    $db = new PDO('mysql:host='.$db_host.';dbname='.$db_name.';charset=utf8', $db_user, $db_passwd, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+} catch(PDOException $e) {
+    die("Connection failed: ".$e->getMessage());
+    // Le die va arrête l'execution d'autres lignes php qui suivent
+    // On pourrait faire en sorte qu'à la place, l'utilisateur soit redirigé vers une page d'erreur
+    // s/o : https://stackoverflow.com/questions/8665985/php-utilizing-exit-or-die-after-headerlocation
+    // Ex:
+    // header("Location: error.php");
+    // exit();
 }
 
 ?>
--- src/includes/login.php
+++ src/includes/login.php
@@ -3,6 +3,13 @@ session_start();
 include("database.php");
 
 if(isset($_POST['login']) and isset($_POST['password'])) {
+
+    $connexion_valide = true;
+
+    if($connexion_valide){
+        $_SESSION['logged_in'] = true;
+    }
+
 }
 
 ?>
