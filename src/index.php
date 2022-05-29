19c29d90b7fde3dfafd6e9d3a0101517eade3677
Yvan <dev@yvan.dev>
2022-05-29 21:15:00 +0000

    on retire les commentaires pour testeré

--- src/index.php
+++ src/index.php
@@ -5,7 +5,7 @@ $description="Page d'accueil du site komposant.com";
 ?>
 
 <html>
-    <!--- Balise head pour changer le titre de la page et la description -->
+
     <?php include("includes/navbar.php"); ?>
     <div>Accueil du site</div>
     <?php include("includes/header.php") ?>
