6b2e3ec58ad637dcc5af33e22a9683fc76f736e5
Yvan <dev@yvan.dev>
2022-05-31 13:47:40 +0000

    debut page compte

---
+++ src/account.php
@@ -0,0 +1,13 @@
+<?php
+$title = "Mon compte - Komposant";
+$description = "Accédez à votre compte komposant.com";
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <?php include("includes/header.php"); ?>
+    <body>
+        <?php include("includes/navbar.php"); ?>
+    </body>
+</html>
