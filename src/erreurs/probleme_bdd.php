467a6d5b08242db9fc048aaed61ff209b1169ad9
Yvan <dev@yvan.dev>
2022-06-02 09:10:43 +0000

    ajout des erreurs

---
+++ src/erreurs/probleme_bdd.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Erreur à afficher en cas de problème lors de la connexion à la base de données
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <body>
+        <div>Une erreur a eu lieu lors de la connexion à la base de données</div>
+    </body>
+</html>
