dff31ab0012a06f37dc28e81b56facc055dc01ff
Yvan <dev@yvan.dev>
2022-06-03 07:05:41 +0000

    modifications register + login

--- src/includes/login.php
+++ src/includes/login.php
@@ -21,7 +21,7 @@ if(isset($_POST['login']) and isset($_POST['password'])) {
     if($connexion_valide){
         /* On ajoute les détails de connexion dans la session de l'utilisateur */
         $_SESSION['connected'] = true;
-        $_SESSION['username'] = $result['mail'];
+        $_SESSION['username'] = $result['email'];
         $_SESSION['password'] = $password;
         echo "\n Connecté";
     } else {
--- src/includes/register.php
+++ src/includes/register.php
@@ -12,9 +12,8 @@ if(isset($_POST['login']) and isset($_POST['password']) and isset($_POST['email'
         
         /* Insérer dans la base de données le nouvel utilisateur si les conditions sont respectées */
 
-        $req = $db->prepare('INSERT INTO users (login, password, email) VALUES (:login, :password, :email)');
+        $req = $db->prepare('INSERT INTO users (password, email) VALUES (:password, :email)');
         $req->execute(array(
-            'login' => $_POST['login'],
             'password' => password_hash($_POST['password'], PASSWORD_BCRYPT),
             'email' => $_POST['email']
         ));
