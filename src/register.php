2773db5109d4962fa12a2beda85d9af9959845ff
Theo BERNARD <theo2.bernard@etu.u-pec.fr>
2022-06-03 15:07:42 +0200

    page register

--- src/register.php
+++ src/register.php
@@ -4,7 +4,27 @@ $description = "Page pour créer un compte sur le site komposant.com";
 ?>
 
 <html>
+    
     <?php include("includes/header.php"); ?>
 
-    <?php include("includes/navbar.php"); ?>
+    <body>
+
+        <?php include("includes/navbar.php"); ?>
+
+        <form action="includes/register.php" method="post" class="w3-container w3-card">
+            <h2>S'inscrire</h2>
+            <label>Nom</label>
+            <input type="text" name="nom" placeholder="Nom" class="w3-input" required>
+            <label>Prénom</label>
+            <input type="text" name="prenom" placeholder="Prénom" class="w3-input" required>
+            <label>Adresse mail</label>
+            <input type="mail" name="login" placeholder="Adresse mail" class="w3-input" required>
+            <label>Mot de passe</label>
+            <input type="password" name="password" placeholder="Mot de passe" class="w3-input" required>
+            <label>Confirmer votre mot de passe</label>
+            <input type="password" name="passwordverif" placeholder="Entrer à nouveau votre mot de passe" class="w3-input" required>
+            <br>
+            <input type="submit" value="Login" class="w3-button w3-green">
+        </form>
+
 </html>
