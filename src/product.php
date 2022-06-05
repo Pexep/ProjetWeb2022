86a4a851c94e01a5eae430a47e8560d515981cb3
Yvan <dev@yvan.dev>
2022-06-05 13:49:15 +0000

    Modifications product.php

--- src/product.php
+++ src/product.php
@@ -3,22 +3,21 @@ include("includes/before_headers.php");
 
 $found = false;
 
-
-/* */
+/* On change la description du site en fonction de si le produit a été trouvé ou non */
 if(!$found){
     /* 
     On précise que le produit n'a pas été trouvé.
     */
     $title = "Produit non trouvé - Komposant";
     $description = "Ce produit n'existe pas.";
-}
-else {
+} else {
     /* 
     On peut mettre les informations du produit dans la description et le titre de la page si on les trouve bien.
     */
     $title = "Nom produit - composant";
     $description = "Description du produit";
 }
+
 ?>
 <html>
     <?php include("includes/header.php"); ?>
