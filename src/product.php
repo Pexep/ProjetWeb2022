f104af59c5c4413ea7067fbe6b8644258bdaa4cd
Yvan <dev@yvan.dev>
2022-06-05 17:55:13 +0000

    avancement sur les produits

--- src/product.php
+++ src/product.php
@@ -3,6 +3,17 @@ include("includes/before_headers.php");
 
 $found = false;
 
+if(isset($_GET["productID"])){
+    $productID = $_GET["productID"];
+    $req = $db->prepare("SELECT * FROM products WHERE id = ?");
+    $req->execute(array($productID));
+    $product = $req->fetch();
+    if($product != false){
+        $found = true;
+    }
+}
+
+
 /* On change la description du site en fonction de si le produit a été trouvé ou non */
 if(!$found){
     /* 
@@ -14,8 +25,8 @@ if(!$found){
     /* 
     On peut mettre les informations du produit dans la description et le titre de la page si on les trouve bien.
     */
-    $title = "Nom produit - composant";
-    $description = "Description du produit";
+    $title = $product["name"];
+    $description = $product["description"];
 }
 
 ?>
