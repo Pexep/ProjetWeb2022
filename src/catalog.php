0d91b3bd252731e1bed2c412da20653765e4a923
Yvan <dev@yvan.dev>
2022-06-07 19:07:46 +0000

    products: début du travail sur le catalogue

---
+++ src/catalog.php
@@ -0,0 +1,18 @@
+<?php
+
+include("includes/before_headers.php");
+
+$title = "Nos produits";
+$description = "Nos produits disponibles sur notre site";
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+    <?php include("includes/header.php"); ?>
+    <body>
+        <?php include("includes/navbar.php"); ?>
+        <h1>Nos produits</h1>
+    </body>
+</html>
