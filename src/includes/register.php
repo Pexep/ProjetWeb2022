85fa29101e5ebcca35113025fdcc50f591e3c0d6
Theo BERNARD <theo2.bernard@etu.u-pec.fr>
2022-06-07 13:21:59 +0200

    changement isset register

--- src/includes/register.php
+++ src/includes/register.php
@@ -1,7 +1,7 @@
 <?php
 include("database.php");
 
-if(isset($_POST['login']) and isset($_POST['password']) and isset($_POST['email'])){
+if(isset($_POST['nom']) and isset($_POST['prenom']) and isset($_POST['password']) and isset($_POST['email'])){
 
     $insertion_possible = false;
 
