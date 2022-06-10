a10f79c84effe7424702a4b4498eacee3d3790e4
Theo BERNARD <theo2.bernard@etu.u-pec.fr>
2022-06-10 15:44:03 +0200

    ajout d'une icone de telephone pour test dans le catalogue

--- src/catalog.php
+++ src/catalog.php
@@ -28,7 +28,7 @@ else {
         <?php
         if($display_categories){
             foreach($list_cat as $cat){
-                ?><div class="w3-quarter">
+                ?><div class="w3-card-4">
                     
                     <?php echo "<img src=\"".echo $cat["image"]."\">";
                     echo "<a href='?categorie=".$cat["id"]."'>".$cat["title"]."</a><br>";
