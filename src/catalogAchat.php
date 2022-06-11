7be7cf658a69a0303e7824e20f3ec5d10bd23ed6
chaignea <mathis.chaigneau@etu.u-pec.fr>
2022-06-11 12:47:53 +0200

    Test non affichage produits non vendus/en stock

--- src/catalogAchat.php
+++ src/catalogAchat.php
@@ -2,12 +2,12 @@
 include("includes/before_headers.php");
 
 $title = "Nos produits";
-$description = "Nos produits disponibles sur notre site";
+$description = "Nos produits disponibles à l'achat sur notre site";
 $display_categories = true;
 
 if(isset($_GET["categorie"])){
     $display_categories = false;
-    $products = $db->prepare("SELECT * from products WHERE category = ?");
+    $products = $db->prepare("SELECT p.id, p.name, p.image,  from products p JOIN businessSell b ON p.id=b.product WHERE category = ?");
     $products->execute(array($_GET["categorie"]));
 }
 else {
