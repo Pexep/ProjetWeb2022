42dc8da20d54d034077d2b2fa881fe88eb5b0a6d
Theo BERNARD <theo2.bernard@etu.u-pec.fr>
2022-06-14 11:46:27 +0200

    ajout économie circulaire sur page accueil

--- src/index.php
+++ src/index.php
@@ -14,10 +14,21 @@ $description="Page d'accueil du site komposant.com";
         <br>
         
         <div class="w3-center">
-        	<img src="images/komposant_logo.png" alt="Logo Komposant" width="400" height="400">
+        	<img src="images/komposant_logo.png" alt="Logo Komposant" width="300" height="300">
         	<div class="w3-container">
         		<h2>Bienvenue sur notre site Komposant !</h2>
         	</div>
+        </div>
+        
+        <br>
+
+        <div class="w3-center">
+        	
+        	<div class="w3-container">
+        		<h1>Notre objectif est de réduire l'impact environnemental de l'informatique. <br>Pour cela, nous appliquons une stratégie d'économie circulaire </h1>
+                <h1>et collaborons avec <br>nos partenaires, </h1>
+        	</div>
+            <img src="images/economie_circulaire.jpg" alt="Economie Circulaire">
         </div>
 
 
