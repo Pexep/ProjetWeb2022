f9e5117c2e41b47e5a840fc795a35513f1606c15
Yvan <dev@yvan.dev>
2022-06-14 08:24:02 +0000

    Navbar: fix sur le dessus de la page

--- src/includes/navbar.php
+++ src/includes/navbar.php
@@ -6,33 +6,34 @@ $path = $_SERVER['REQUEST_URI'];
 $path = urlencode($path);
 $connected = isset($_SESSION['connected']) && $_SESSION['connected'] == true;
 ?>
-
-<div class="w3-bar w3-light-grey">
-    <a href="index.php" class="w3-bar-item w3-button">Komposant</a>
-    <a href="catalogAchat.php" class="w3-bar-item w3-button">Acheter</a>
-    <a href="catalogVente.php" class="w3-bar-item w3-button">Vendre</a>
-    <div class="w3-dropdown-hover">
-        <button class="w3-button">À propos</button>
-        <div class="w3-dropdown-content w3-bar-block w3-card-4">
-            <a href="team.php" class="w3-bar-item w3-button">Notre équipe</a>
-            <a href="partners.php" class="w3-bar-item w3-button">Nos partenaires</a>
-            <a href="#" class="w3-bar-item w3-button">Link 3</a>
-        </div>
-    </div>
-    <?php
-    if ($connected) {
-    ?>
-        <div class="w3-dropdown-hover w3-right">
-            <a class="w3-button" href="infoUser.php"><?php echo $_SESSION["fullname"]; ?></a>
+<div class="w3-top">
+    <div class="w3-bar w3-light-grey">
+        <a href="index.php" class="w3-bar-item w3-button">Komposant</a>
+        <a href="catalogAchat.php" class="w3-bar-item w3-button">Acheter</a>
+        <a href="catalogVente.php" class="w3-bar-item w3-button">Vendre</a>
+        <div class="w3-dropdown-hover">
+            <button class="w3-button">À propos</button>
             <div class="w3-dropdown-content w3-bar-block w3-card-4">
-                <a href="infoUser.php" class="w3-bar-item w3-button">Mon compte</a>
-                <a href="includes/logout.php" class="w3-bar-item w3-button">Déconnexion</a>
+                <a href="team.php" class="w3-bar-item w3-button">Notre équipe</a>
+                <a href="partners.php" class="w3-bar-item w3-button">Nos partenaires</a>
+                <a href="#" class="w3-bar-item w3-button">Link 3</a>
             </div>
         </div>
-
-    <?php
-    } else {
-        echo '<a href="login.php?redirect_to=' . $path . '" class="w3-bar-item w3-button w3-right">Se connecter</a>';
-    }
-    ?>
+        <?php
+        if ($connected) {
+        ?>
+            <div class="w3-dropdown-hover w3-right">
+                <a class="w3-button" href="infoUser.php"><?php echo $_SESSION["fullname"]; ?></a>
+                <div class="w3-dropdown-content w3-bar-block w3-card-4">
+                    <a href="infoUser.php" class="w3-bar-item w3-button">Mon compte</a>
+                    <a href="includes/logout.php" class="w3-bar-item w3-button">Déconnexion</a>
+                </div>
+            </div>
+    
+        <?php
+        } else {
+            echo '<a href="login.php?redirect_to=' . $path . '" class="w3-bar-item w3-button w3-right">Se connecter</a>';
+        }
+        ?>
+    </div>
 </div>
